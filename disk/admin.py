5563a7e97c5cf6a9f2720ae935baa716a6a4663f
gracy.ma <glmgracy@gmail.com>
2017-11-30 13:30:57 +0800

    添加访问限制

--- disk/admin.py
+++ disk/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserGPS
+
+admin.site.register(UserGPS)
 
 # Register your models here.
--- disk/models.py
+++ disk/models.py
@@ -9,3 +9,19 @@ class FileInfo(models.Model):
 	created_date = models.DateTimeField()
 	md5 = models.CharField(max_length=32, null=False)
 	user_ip = models.CharField(max_length=40, default='255.255.255.0')
+
+class UserGPS(models.Model):
+	username = models.CharField(max_length=50, null=True)
+	user_ip = models.GenericIPAddressField(null=True)
+	user_agent = models.CharField(max_length=200, null=True)
+	lng = models.CharField(max_length=15, null=True)
+	lat = models.CharField(max_length=15, null=True)
+	address = models.CharField(max_length=200, null=True)
+	code = models.CharField(max_length=50, null=True)
+	quota = models.IntegerField(null=True)
+	use = models.IntegerField(null=True)
+	create_date = models.DateTimeField(auto_now_add = True, null=True)
+	create_by = models.CharField(max_length=50, null=True)
+
+	def __str__(self):
+		return self.username
